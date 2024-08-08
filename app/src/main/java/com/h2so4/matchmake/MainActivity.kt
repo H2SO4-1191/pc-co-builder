f9dad638825c7f97c4a07ad5b2e3af8eb67ae39d
H2SO4-1191 <H2SO4.1191@GMAIL.COM>
2024-08-08 14:16:41 +0300

    Transition and other changes in drawables

--- app/src/main/java/com/h2so4/matchmake/MainActivity.kt
+++ app/src/main/java/com/h2so4/matchmake/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.h2so4.matchmake
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.h2so4.matchmake.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        val ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        ui.build.setOnClickListener{
+            val purposeIntent = Intent(this, Purpose::class.java)
+            startActivity(purposeIntent)
+        }
     }
 }
