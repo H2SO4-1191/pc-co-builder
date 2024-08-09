1a5934a1785269afd92988e60c7a461b3c9ec867
H2SO4-1191 <H2SO4.1191@GMAIL.COM>
2024-08-09 10:57:16 +0300

    Double check

---
+++ app/src/main/java/com/h2so4/matchmake/BudgetActivity.kt
@@ -0,0 +1,10 @@
+package com.h2so4.matchmake
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class BudgetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_budget)
+
+    }
+}
